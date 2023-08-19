1a85925ba37371c5a013b39fd3ba70f0064e2990
Fernanda Higuti <fernandamh1996@gmail.com>
2023-08-18 21:17:04 -0300

    admin product management

--- pages/products.js
+++ pages/products.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Products() {
   return ( 
   <Layout>
-    <Link className="bg-blue-900 text-white rounded-md py-1 px-2" href={'/products/new'}>
+    <Link className="btn-primary" href={'/products/new'}>
       Add new product
     </Link>
   </Layout>
---
+++ pages/products/new.js
@@ -0,0 +1,42 @@
+import Layout from "@/components/Layout";
+import {useState} from "react";
+import axios from "axios";
+
+export default function NewProduct() {
+  
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+
+  async function createProduct(ev){
+    ev.preventDefault();
+    const data = {title, description, price};
+    await axios.post('/api/products', data);
+  }
+
+ return(
+  <Layout>
+    <form onSubmit={createProduct}>
+      <h1>New Product</h1>
+      <label >Product price</label>
+      <input 
+        type="text" 
+        placeholder="product name"
+        value={title}
+        onChange={ev => setTitle(ev.target.value)}/>
+      <label >Product description</label>
+      <textarea 
+        placeholder="description"
+        value={description}
+        onChange={ev => setDescription(ev.target.value)}/>
+       <label >Product price</label>
+     <input 
+        type="number" 
+        placeholder="price"
+        value={price}
+        onChange={ev => setPrice(ev.target.value)}/>
+      <button type="submit" className="btn-primary">Save</button>
+    </form>
+  </Layout>
+ )
+}
