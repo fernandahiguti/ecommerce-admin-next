7a952c9848f4672b5e3b109a17e9e1429baa5221
Fernanda Higuti <fernandamh1996@gmail.com>
2023-08-19 21:50:29 -0300

    add upload product image feature

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -39,8 +39,11 @@ export default function ProductForm({
       for (const file of files) {
         data.append('file', file);
       }
-      const res = await axios.post('/api/upload', data);
-      console.log(res.data);
+     const res = await fetch('/api/upload', {
+        method: 'POST',
+        body: data
+      })
+      console.log(res);
     }
   }
 
--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,4 +1,8 @@
 import multiparty from 'multiparty';
+import {PutObjectCommand, S3Client} from '@aws-sdk/client-s3';
+import fs from 'fs';
+import mime from "mime-types";
+const bucketName = 'fernanda-nextjs-ecommerce';
 
 export default async function handle(req, res){
   const form = new multiparty.Form();
@@ -8,13 +12,30 @@ export default async function handle(req, res){
       resolve({fields, files});
     });
  });  
-    console.log('length:', files.length);
-    console.log(fields);
-    return res.json('ok')
+    const client = new S3Client({
+      region: 'us-east-2',
+      credentials: {
+        accessKeyId: process.env.S3_ACCESS_KEY,
+        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+      }
+    });
+    const links = [];
+    for (const file of files.file) {
+      const ext = file.originalFilename.split('.').pop();
+      const newFilename = Date.now() + '.' + ext;
+      await client.send(new PutObjectCommand({
+        Bucket: bucketName,
+        Key: newFilename,
+        Body: fs.readFileSync(file.path),
+        ACL: 'public-read',
+        ContentType: mime.lookup(file.path),
+      }));
+      const link = `https://${bucketName}.s3.amazonaws.com/${newFilename}`;
+      links.push(link);
+    }
+    return res.json({links});
+  }
 
-}
-
-
-export const config={
+export const config = {
   api: {bodyParser:false},
 };
