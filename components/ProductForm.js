7b9328bbef1fe41d288b3c23496f162a584e145d
Fernanda Higuti <fernandamh1996@gmail.com>
2023-09-07 14:57:14 -0300

    connect categories to products

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import axios from "axios";
 import {useRouter} from "next/router";
 import Spinner from "./Spinner";
@@ -10,19 +10,28 @@ export default function ProductForm({
   description: existingDescription, 
   price: existingPrice,
   images: existingImages,
+  category: assignedCategory,
 }) {
 
   const [title, setTitle] = useState(existingTitle || '');
   const [description, setDescription] = useState(existingDescription || '');
   const [price, setPrice] = useState(existingPrice || '');
+  const [category, setCategory] = useState(assignedCategory || '');
   const [images, setImages] = useState(existingImages || []);
   const [goToProducts, setGoToProducts] = useState(false);
-  const [isUploading, setIsUploading] = useState(false)
+  const [isUploading, setIsUploading] = useState(false);
+  const [categories, setCategories] = useState([]);
   const router = useRouter();
+
+  useEffect(() => {
+    axios.get('/api/categories').then(result => {
+      setCategories(result.data);
+    })
+  }, [])
 
   async function saveProduct(ev){
     ev.preventDefault();
-    const data = {title, description, price, images};
+    const data = {title, description, price, images, category};
 
     if(_id){
       await axios.put('/api/products', {...data,_id});
@@ -64,6 +73,18 @@ export default function ProductForm({
         placeholder="product name"
         value={title}
         onChange={ev => setTitle(ev.target.value)}/>
+
+      <label>Category</label>
+      <select 
+        value={category}
+        onChange={ev => setCategory(ev.target.value)}
+
+      >
+        <option value="">No category</option>
+        {categories.length > 0 && categories.map(c => (
+          <option value={c._id}>{c.name}</option>
+        ))}
+      </select>
       
       <label>
         Photos
